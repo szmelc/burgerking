b104f4f230be801573944d639e24a102ec56ee16
Lukas <lukasz.szmelc@netguru.pl>
2018-04-07 21:50:00 +0200

    Add navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 import classes from './Layout.css'
 
 const layout = ( props ) => (
   <Aux>
+    <Toolbar />
     <div>Toolbar, SideDrawer, Backdrop</div>
     <main className={classes.Content}>
       {props.children}
