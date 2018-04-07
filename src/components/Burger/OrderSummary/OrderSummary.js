1d105e6995cd623268023ba5db64e8a5c8005189
Lukas <lukasz.szmelc@netguru.pl>
2018-04-07 20:43:31 +0200

    Add checkout

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button'
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients)
@@ -13,12 +14,15 @@ const orderSummary = (props) => {
     });
   return (
     <Aux>
-      <h3>Your Order:</h3>
+      <h3>Your Order</h3>
       <p>Delicious burger with the following ingredients</p>
       <ul>
         {ingredientSummary}
       </ul>
+      <p><strong>Total Price:</strong> ${props.price.toFixed(2)}</p>
       <p>Continue to Checkout?</p>
+      <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
     </Aux>
   )
 };
